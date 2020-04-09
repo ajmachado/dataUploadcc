78bc4115f01f79d792e1dda88a8cf4d6c97e55c2
ajmachado <machadav@merck.com>
2020-04-09 15:47:08 -0400

    Add test file

---
+++ data_chaincode_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	chainCodeID      = "dataUpload_1"
+	
+	mockDevJson = `{"id":1,"gtin":"08806555018611","lot":"M036191","serialNo":"1936800","expirationDate":"10/10/2026","event":"commission","gln":"0300060000037","status":"active","tradeItemDesc":"gardasil9 10 pack ","product":"gardasil9","tradename":"Gardasil 9","manufactureDate":"10/10/2019","location":"Wilson, NC","toGln":"0300060000037","sender":"manufacturer","receiver":"manufacturer"}`
+	
+)
+
+// If TestMain exists then this will run all the tests,
+// there can be a setup before all tests and a clean up after all tests have run
+// NOTE: our tests will use the logger declared globally in the chaincode
+func TestMain(m *testing.M) {
+	//logger.SetLevel(shim.LogWarning)
+	// logger.SetLevel(shim.LogDebug)
+
+	//logger.Debug("TestMain: enter")
+	//defer logger.Debug("TestMain: exit")
+	fmt.println("TestMain");
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
+
+} // end of TestMain
+
+func TestCreateProducts(t *testing.T) {
+	fmt.println("TestCreateProducts: enter")
+	
+	stub := shim.NewMockStub("mockStub", new(DataChainCode))
+
+	if stub == nil {
+		t.Fatalf("MockStub creation failed")
+	}
+	results := stub.MockInvoke("TestCreateproducts", [][]byte{[]byte("createProduct"), []byte(mockDevJson)})
+	var returnCode = int(results.Status)
+	assert.Equal(t, 200, returnCode, "Result : Success")
+
+	
+} // end of TestCreateProducts
+
+func TestCRUD(t *testing.T) {
+	fmt.println("TestCRUD: enter")
+	//defer logger.Debug("TestCRUD: exit")
+
+	stub := shim.NewMockStub("mockStub", new(DataChainCode))
+
+	if stub == nil {
+		t.Fatalf("MockStub creation failed")
+	}
+
+	results := stub.MockInvoke("TestCRUD", [][]byte{[]byte("createProduct"), []byte(mockDevJson)})
+	var returnCode = int(results.Status)
+	assert.Equal(t, 200, returnCode, "Result : Success")
+} // end of TestCRUD
+
